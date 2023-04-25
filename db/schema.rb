5d5028bb56e5c14fcf58ea76b566a23b152c06ec
PrajaktaShelke <prajakta.shelke20188@gmail.com>
2023-04-25 09:10:39 +0530

    generate model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_25_033029) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_25_033913) do
+  create_table "students", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.integer "mobileno"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
